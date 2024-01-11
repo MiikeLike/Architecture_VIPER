441d60cd6da5e0b5f973a308c21cefd6b415c2df
MiikeLike <mak_valle@hotmail.es>
2024-01-11 21:53:18 +0100

    Update DetailView.swift
    Creación de las tres subViews para mostrar detalles de la película

--- Architecture_VIPER/DetailMoview/DetailView.swift
+++ Architecture_VIPER/DetailMoview/DetailView.swift
@@ -11,6 +11,29 @@ import UIKit
 class DetailView: UIViewController {
     private let presenter: DetailPresentable
     
+    let movieImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode  = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let movieName: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.font = .systemFont(ofSize: 32, weight: .bold, width: .condensed)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()   
+    
+    let movieDescription: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 22, weight: .bold, width: .standard)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     init(presenter: DetailPresentable) {
         self.presenter = presenter
         super.init(nibName: nil, bundle: nil)
