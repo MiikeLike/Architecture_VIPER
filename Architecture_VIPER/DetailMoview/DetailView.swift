19f5286355ccd5302281eb56ac3b775430cb9928
MiikeLike <mak_valle@hotmail.es>
2024-01-11 21:44:39 +0100

    Update DetailView.swift
    Creamos una referencia al presenter para conseguir desacoplar de una implementación concreta.

--- Architecture_VIPER/DetailMoview/DetailView.swift
+++ Architecture_VIPER/DetailMoview/DetailView.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+class DetailView: UIViewController {
+    private let presenter: DetailPresentable
+    
+    init(presenter: DetailPresentable) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
