e38df1720944cf146d4c7cb429e8fe57c24b5bdb
MiikeLike <mak_valle@hotmail.es>
2024-01-13 20:36:39 +0100

    Update DetailRouter.swift
    Creación metodo para unir todos los componentes de VIPER View, Presenter y interactor

--- Architecture_VIPER/DetailMoview/DetailRouter.swift
+++ Architecture_VIPER/DetailMoview/DetailRouter.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class DetailRouter {
+    func showDetail(withMovieId movieId : String) {
+        let interactor = DetailInteractor()
+        let presenter = DetailPresenter(movieId: movieId,
+                                        interactor: interactor,
+                                        mapper: MapperDetailMovieViewModel())
+        let view = DetailView()
+    }
+}
